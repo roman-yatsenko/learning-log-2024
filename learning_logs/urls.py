ca722153152bf07252d7ac5aea37e723b2f1f7e4
YaRo <roman.yatsenko@hneu.net>
2024-05-18 15:25:28 +0300

    Add topic page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Сторінка з переліком всіх тем
     path('topics/', views.topics, name='topics'),
+    # Сторінка з докладною інформацією за окремою темою
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
