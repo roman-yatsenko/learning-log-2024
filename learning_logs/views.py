1af7554e0db695b260666e6cbe930e4992bd9b77
YaRo <roman.yatsenko@hneu.net>
2024-05-25 15:21:19 +0300

    Add edit entry page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
 from .forms import EntryForm, TopicForm
-from .models import Topic
+from .models import Topic, Entry
 
 # Create your views here.
 
@@ -52,3 +52,19 @@ def new_entry(request, topic_id):
         
     context = {'form': form, 'topic': topic}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Редагує існуючу нотатку"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+        
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
